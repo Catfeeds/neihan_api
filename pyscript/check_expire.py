da773d5f5136050eb1459d46a0e6cc0b13940692
望月 <lupt059@163.com>
2017-11-14 09:21:32 +0800

    fix bug

--- pyscript/check_expire.py
+++ pyscript/check_expire.py
@@ -16,33 +16,36 @@ _mgr = Mgr(create_engine(_db_url, pool_recycle=10))
 
 
 def check(video):
-    logging.info('check video is expired: {}'.format(video.content.encode('utf8')))
-    url = video.vurl
-    r = requests.head(url, allow_redirects=True, timeout=10)
-    if r and r.status_code != 200:
-        print r
-        return None
-    for h in r.history:
-        if '22000000000000000000000000000000000' in h.headers['Location']:
-            logging.info('this video is expired: {}'.format(video.content.encode('utf8')))
-            _mgr.update_video_expired(video.id)
-            break
+    try:
+        logging.info('check video is expired: {}'.format(video.content.encode('utf8')))
+        url = video.vurl
+        r = requests.head(url, allow_redirects=True, timeout=10)
+        if r and r.status_code != 200:
+            print r
+            return None
+        for h in r.history:
+            if '22000000000000000000000000000000000' in h.headers['Location']:
+                logging.info('this video is expired: {}'.format(video.content.encode('utf8')))
+                _mgr.update_video_expired(video.id)
+                break
+    except:
+        pass
 
 
 def main():
-    for i in xrange(1000):
+    for i in xrange(1000000):
         params = {
-            'category': [65, 1111],
+            'category': [65],
             'is_expired': 0,
-            'limit': 1000,
-            'offset': i*1000
+            'limit': 2,
+            'offset': i*2
         }
         videos = _mgr.get_videos(params)
         if len(videos) == 0:
             break
         pools = Pool(5)
         pools.map(check, videos)
-        sleep(10)
+        sleep(2)
 
 
 if __name__ == '__main__':
