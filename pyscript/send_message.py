af97dad2c8707a364394e452f06c9df0bd704820
望月 <lupt059@163.com>
2017-12-01 10:47:16 +0800

    fix bug

--- pyscript/send_message.py
+++ pyscript/send_message.py
@@ -54,13 +54,12 @@ def send_msg(arg):
 
     formid = formids[0]
     params = {
-        "emphasis_keyword": ["keyword1"],
         "touser": u['openid'].encode('utf8'),
         "template_id": 'Vpq9PCekMsNMr8zQKC6Jpgd0yhfo8a319k7pdC3NvB8',
         "page": "pages/index/index?video_id={}&from_user_id={}".format(video['group_id'].encode('utf8'), video['from_user_id'].encode('utf8')),
         "form_id": formid['form_id'].encode('utf8'),
+        "emphasis_keyword": "keyword1.DATA",
         "data": {
-            "emphasis_keyword": ["keyword1"],
             "keyword1": {
                 "value": video['title'].encode('utf8'),
                 "color": "#FF0000",
