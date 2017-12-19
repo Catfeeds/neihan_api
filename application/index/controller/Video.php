dc6e90ba55bb677553cd89bf38eae82217078ffc
卢平涛 <lupingtao@ACA80202.ipt.aol.com>
2017-12-19 14:27:28 +0800

    fix bug

--- application/index/controller/Video.php
+++ application/index/controller/Video.php
@@ -133,14 +133,24 @@ class Video extends Controller
                     'path' => '',
                     'extraData' => ''
                 ];
-                /*
+                
+
                 if($k == 0) {
-                    $ad['image'] = $request->domain().'/static/image/ad-ldh.jpg';
-                    $ad['appId'] = 'wx564a50039dd91934';
-                    $ad['path'] = 'pages/index/index';
-                    $ad['extraData'] = '';
+                    if($settings['ad_show'] == 1) {
+                        $ad['image'] = $request->domain().'/static/image/ad-ldh.jpg';
+                        $ad['appId'] = 'wx564a50039dd91934';
+                        $ad['path'] = 'pages/index/index';
+                        $ad['extraData'] = '';
+                    } elseif($settings['ad_show'] == 2) {
+                        $ad_show = rand(1, 100);
+                        if($settings['ad_show_ratio'] >= $ad_show) {
+                            $ad['image'] = $request->domain().'/static/image/ad-ldh.jpg';
+                            $ad['appId'] = 'wx564a50039dd91934';
+                            $ad['path'] = 'pages/index/index';
+                            $ad['extraData'] = '';
+                        }
+                    }
                 }
-                */
                 $val['ad'] = $ad;
 
                 if($this->app_code == 'neihan_1') {
