1a3a6972ef4228e779d47d5956f11a2a0663255a
卢平涛 <lupingtao@ACA80202.ipt.aol.com>
2017-12-20 13:27:04 +0800

    fix bug

--- pyscript/send_message_mp.py
+++ pyscript/send_message_mp.py
@@ -87,7 +87,8 @@ def send_msg(u):
 
 def main():
     global total_send
-    uparams = { 'user_id': 17 }
+    # uparams = { 'user_id': 17 }
+    uparams = {}
     users = _mgr.get_users_mp(uparams)
 
     if users:
