3ac8ad1c354300b61a14c4121758e1683d675fae
卢平涛 <lupingtao@ACA80202.ipt.aol.com>
2017-12-26 14:18:14 +0800

    fix bug

--- application/index/controller/Video.php
+++ application/index/controller/Video.php
@@ -56,7 +56,7 @@ class Video extends Base
             }
 
             if(empty($category) && $this->app_code != 'neihan_1') {
-                $category = [187, 1112, 1113];
+                $category = [187, 1112, 1113, 1114];
             }
 
             $data = array('c' => 0, 'm' => '', 'd' => array());
